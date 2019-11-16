d6f78070b5db50e2beef0313d704aac1c495b76c
Bilash <bilashcse@gmail.com>
2019-11-16 19:44:59 +0600

    porichoi validation service added, api implemented

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const {verify} = require('./nid');
+
+const app = express();
+const port = process.env.PORT || 3004;
+app.listen(port);
+
+app.get('/validate', async (req, res) => {
+    const { nidNumber, fullName, dob } = req.query;
+    try {
+        const result = await verify({ nidNumber, fullName, dob });
+        res.json({
+            isError: false,
+            result
+        });
+
+    } catch (err) {
+        console.error(err.message);
+        res.send(`Error: `, err.message);
+    }
+});
+
+app.get('/', async (req, res) => {
+    res.send(`Server Up & Running`);
+});
+
+
+
