0da7ea9e6e5ffa55a3c604b7d784430e4eca8ba8
Bilash <bilashcse@gmail.com>
2019-11-17 00:40:06 +0600

    structure fix, configs added

---
+++ src/nid.js
@@ -0,0 +1,37 @@
+const request = require('request-promise-native');
+const configs = require('../configs/configs');
+
+const verificationEndPoint = 'end_point';
+const subscriptionKey = configs.subscriptionKey;
+
+async function verify({ nidNumber, fullName, dob }) {
+  let response = await request({
+    method: 'POST',
+    qs: {
+      national_id: nidNumber,
+      person_dob: dob,
+      person_fullname: fullName
+    },
+    url: verificationEndPoint,
+    headers: {
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': subscriptionKey
+    },
+    json: true
+  });
+
+  if (typeof response === 'string') {
+    response = response.replace(/'/g, '"');
+    response = JSON.parse(response);
+  }
+
+  if (response.passKyc && (response.passKyc === 'true' || response.passKyc === 'yes')) {
+    return { nid: nidNumber, valid: true };
+  }
+
+  return { nid: nidNumber, valid: false };
+}
+
+module.exports = {
+  verify
+};
